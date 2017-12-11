4d6bfa8ddc0619a41aa4db9200493695282adf91
Tony <18813033799@163.com>
2017-12-11 20:13:57 +0800

    the assignment3 project

---
+++ src/com/sadt/util/MyBatisUtils.java
@@ -0,0 +1,26 @@
+package com.sadt.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class MyBatisUtils {
+	private static SqlSessionFactory sqlSessionFactory;
+
+	public static SqlSession getSqlSession() {
+		try {
+			InputStream is = Resources.getResourceAsStream("mybatis.xml");
+			if (sqlSessionFactory == null) {
+				sqlSessionFactory = new SqlSessionFactoryBuilder().build(is);
+			}
+			return sqlSessionFactory.openSession();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
